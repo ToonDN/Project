a2959d0db240c2791d08878e5dc31b4edb616a54
ToonDN <toon.de.neve1@gmail.com>
2021-03-10 16:53:47 +0100

    pull test

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,30 @@
 #include <util/delay.h>
 #include "dwenguino/dwenguino_board.hpp"
 #include "dwenguino/dwenguino_lcd.hpp"
+#include <avr/interrupt.h>
 
 DwenguinoLCD lcd;
 
 int main(void)
 {
-initBoard();
-lcd = DwenguinoLCD();
-lcd.initLCD();
+  SREG |= (1 << SREG_I);
+  DDRC |= (1 << PC0);
 
-backlightOn();
+  TIMSK1 |= (1 << OCIE1A);
 
-  while(1){
+  TCCR1A &= ~(1 << WGM11);
+  TCCR1A &= ~(1 << WGM10);
+  TCCR1B |= ~(1 << WGM12);
+  TCCR1B &= ~(1 << WGM13);
+
+  initBoard();
+  lcd = DwenguinoLCD();
+  lcd.initLCD();
+
+  backlightOn();
+
+  while (1)
+  {
     lcd.printStringToLCD("Labo 2", 0, 5);
     _delay_ms(1000);
     lcd.clearLCD();
